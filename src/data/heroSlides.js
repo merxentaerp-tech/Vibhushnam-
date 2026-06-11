1d5816d621cc727c6873d097442aa89ee4db7f3c
Siddharth Jha <jhasiddharth495@gmail.com>
2026-06-11 13:52:08 +0530

    Added hero5 image and updated hero slides

--- src/data/heroSlides.js
+++ src/data/heroSlides.js
@@ -2,7 +2,7 @@ import hero1 from "../assets/images/hero/hero1.png";
 import hero2 from "../assets/images/hero/hero2.png";
 import hero3 from "../assets/images/hero/hero3.png";
 import hero4 from "../assets/images/hero/hero4.png";
-
+import hero5 from "../assets/images/hero/hero5.png";
 export const heroSlides = [
   {
     id: 1,
@@ -19,5 +19,9 @@ export const heroSlides = [
   {
     id: 4,
     image: hero4,
+  },
+  {
+    id: 5,
+    image: hero5,
   },
 ];
