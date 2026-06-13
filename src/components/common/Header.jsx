5b5c70cccb4d729f34ebcebe89886e8fd8df65ca
Siddharth Jha <jhasiddharth495@gmail.com>
2026-06-13 11:57:22 +0530

    asdadas

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
@@ -11,6 +12,7 @@ import ringIcon from "../../assets/icons/Ring.svg";
 
 import searchIcon from "../../assets/icons/MagnifyingGlass.svg";
 import micIcon from "../../assets/icons/Microphone.svg";
+import hamburgerIcon from "../../assets/icons/hamburgerIcon.png.png";
 
 const navItems = [
   { id: 1, icon: allIcon, alt: "All", path: "/products" },
@@ -21,18 +23,51 @@ const navItems = [
 ];
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = openMenu ? "hidden" : "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [openMenu]);
+
   return (
     <header className="site-header">
-      <Link to="/" className="logo-link">
-        <img className="header-logo" src={logo} alt="Vibhushanam" />
-      </Link>
+      <div className="header-top">
+        <Link to="/" className="logo-link">
+          <img className="header-logo" src={logo} alt="Vibhushanam" />
+        </Link>
 
-      <nav className="header-nav">
+        <button
+          className="hamburger-btn"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <img src={hamburgerIcon} alt="Menu" />
+        </button>
+      </div>
+
+      <nav className={`header-nav ${openMenu ? "show-menu" : ""}`}>
+        <button className="close-drawer" onClick={() => setOpenMenu(false)}>
+          ×
+        </button>
+
         {navItems.map((item) => (
-          <Link to={item.path} className="nav-item" key={item.id}>
+          <Link
+            to={item.path}
+            className="nav-item"
+            key={item.id}
+            onClick={() => setOpenMenu(false)}
+          >
             <img src={item.icon} alt={item.alt} />
+            <span>{item.alt}</span>
           </Link>
         ))}
+
+        <div className="drawer-bottom-logo">
+          <img src={logo} alt="Vibhushanam" />
+        </div>
       </nav>
 
       <div className="header-search">
